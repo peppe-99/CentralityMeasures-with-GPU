0d19b379f6cb6921713ff81a416bb95ab156ad7b
peppe-99 <g.cardaropoli99@gmail.com>
2023-04-20 10:44:01 +0200

    fix an error

--- CentralityMeasures/ClosenessCentrality/closeness-centrality.c
+++ CentralityMeasures/ClosenessCentrality/closeness-centrality.c
@@ -3,37 +3,35 @@
 #include<stdlib.h>
 #include<string.h>
 
+/* Funzione per leggere una matrice da un file */
 int readMatrix(int rows, int cols, int *matrix, const char *filename);
 
-void swap(int **current_matrix, int **new_matrix) {
-    int *temp = *current_matrix;
-    *current_matrix = *new_matrix;
-    *new_matrix = temp;
-}
+/* Funzione per lo swap di matrici */
+void swap(int **current_matrix, int **new_matrix);
 
 int main(int argc, char const *argv[]) {
 
-    int node, rows, cols;
+    int node, rows, cols, rimanenti;
     int *matrix, *pwd_matrix, *tmp_matrix, *distance_matrix;
     double *closeness_centrality;
 
+    /* Input: numero di nodi del grafo */
     printf("Inserire numero di nodi: ");
     scanf("%d", &node);
     rows = node;
     cols = node;
-    int rimanenti = (node * node) - node;
 
-    // Allochiamo le strutture dati necessarie
+    /* Allochiamo le strutture dati necessarie */
     matrix = (int*)malloc(rows * cols * sizeof(int));
     tmp_matrix = (int*)malloc(rows * cols * sizeof(int));
     pwd_matrix = (int*)malloc(rows * cols * sizeof(int));
     distance_matrix = (int*)malloc(rows * cols * sizeof(int));
     closeness_centrality = (double*)malloc(node * sizeof(double));
 
-    // Leggiamo la matrice di esempio da un file
+    /* Leggiamo la matrice di esempio da un file */
     readMatrix(rows, cols, matrix, "matrix.dat");
 
-    // Stampiamo la matrice di esempio
+    /* Stampiamo la matrice di esempio */
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%d\t", matrix[i * cols + j]);
@@ -41,7 +39,8 @@ int main(int argc, char const *argv[]) {
         printf("\n");
     }
 
-    // Calcoliamo la distance matrix
+    /* Calcoliamo la distance matrix */
+    rimanenti = (node * node) - node; // numero di celle della distance matrix da valorizzare
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             distance_matrix[i * cols + j] = matrix[i * cols + j];
@@ -49,7 +48,8 @@ int main(int argc, char const *argv[]) {
             if (matrix[i * cols + j] != 0) rimanenti--;
         }
     }
-    while (rimanenti != 0) {
+    /* Calcoliamo le potenze della matrice di adiacenza finché non abbiamo ottenuto la distance matrix */
+    for (int pwd = 2; rimanenti != 0; pwd++) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 pwd_matrix[i * cols + j] = 0;
@@ -57,7 +57,7 @@ int main(int argc, char const *argv[]) {
                     pwd_matrix[i * cols + j] += tmp_matrix[i * cols + k] * matrix[k * cols + j];
                 }
                 if(i != j && pwd_matrix[i * cols + j] != 0 && distance_matrix[i * cols + j] == 0) {
-                    distance_matrix[i * cols + j] = pwd_matrix[i * cols + j];
+                    distance_matrix[i * cols + j] = pwd;
                     rimanenti--;
                 }
             }
@@ -65,7 +65,7 @@ int main(int argc, char const *argv[]) {
         swap(&pwd_matrix, &tmp_matrix);
     }
 
-    // Stampiamo la distance matrix
+    /* Stampiamo la distance matrix */
     printf("\nDistance Matrix:\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -74,13 +74,12 @@ int main(int argc, char const *argv[]) {
         printf("\n");
     }
 
-    //Calcoliamo la Closeness Centrality
+    /* Calcoliamo la Closeness Centrality */
     printf("\nCloseness Centrality:\n");
     for (int i = 0; i < node; i++) {
         int sum_dist = 0;
         for (int j = 0; j < cols; j++) {
             sum_dist += distance_matrix[i * cols + j];
-            printf("sum_dist: %d\n", sum_dist);
         }
         closeness_centrality[i] = (double) 1 / sum_dist;
         printf("Node: %d\tScore: %f\n", (i+1), closeness_centrality[i]);
@@ -104,4 +103,10 @@ int readMatrix(int rows, int cols, int *matrix, const char *filename) {
 
     fclose(file);
     return 1;
+}
+
+void swap(int **current_matrix, int **new_matrix) {
+    int *temp = *current_matrix;
+    *current_matrix = *new_matrix;
+    *new_matrix = temp;
 }
