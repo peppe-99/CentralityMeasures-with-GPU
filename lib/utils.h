170903b7a8e95c8f8ff40ee8879628deb677919a
peppe-99 <g.cardaropoli99@gmail.com>
2023-04-26 11:48:40 +0200

    add a new function

--- lib/utils.h
+++ lib/utils.h
@@ -1,3 +1,4 @@
 /* utils.h */
 extern int readMatrix(int rows, int cols, int *matrix, const char *filename);
 extern void swap(int **current_matrix, int **new_matrix);
+extern void printMatrix(int rows, int cols, int *matrix);
