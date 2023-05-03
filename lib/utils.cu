5e68af83f671deaabaec403e31f53ec8c8d1e570
peppe-99 <g.cardaropoli99@gmail.com>
2023-05-03 18:58:41 +0200

    updated utils files with a new function

--- lib/utils.cu
+++ lib/utils.cu
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include"utils.h"
 
 /* Funzioni per leggere una matrice da un file*/
@@ -62,4 +63,37 @@ void printDMatrix(int rows, int cols, double *matrix) {
         }
         printf("\n");
     }
+}
+
+void distanceMatrix(int rows, int cols, int *matrix, int *distance_matrix) {
+
+    int *tmp_matrix = (int*)malloc(rows * cols * sizeof(int));
+    int *pwd_matrix = (int*)malloc(rows * cols * sizeof(int));
+    int rimanenti = (rows * cols) - rows;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            distance_matrix[i * cols + j] = matrix[i * cols + j];
+            tmp_matrix[i * cols + j] = matrix[i * cols + j];
+            if (matrix[i * cols + j] != 0) rimanenti--;
+        }
+    }
+
+    for (int pwd = 2; rimanenti != 0; pwd++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                pwd_matrix[i * cols + j] = 0;
+                for (int k = 0; k < cols; k++) {
+                    pwd_matrix[i * cols + j] += tmp_matrix[i * cols + k] * matrix[k * cols + j];
+                }
+                if (i != j && pwd_matrix[i * cols + j] != 0 && distance_matrix[i * cols + j] == 0) {
+                    distance_matrix[i * cols + j] = pwd;
+                    rimanenti--;
+                }
+            }
+        }
+        swap(&pwd_matrix, &tmp_matrix);
+    }
+    free(tmp_matrix);
+    free(pwd_matrix);
 }
--- lib/utils.h
+++ lib/utils.h
@@ -1,7 +1,7 @@
 /* utils.h */
 extern int readIMatrix(int rows, int cols, int *matrix, const char *filename);
 extern int readDMatrix(int rows, int cols, double *matrix, const char *filename);
-extern void swap(int **current_matrix, int **new_matrix);
 extern void printIMatrix(int rows, int cols, int *matrix);
 extern void printDMatrix(int rows, int cols, double *matrix);
-extern void printVoidMatrix(int rows, int cols, void **matrix);
+extern void swap(int **current_matrix, int **new_matrix);
+extern void distanceMatrix(int rows, int cols, int *matrix, int *distance_matrix);
