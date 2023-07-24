40146898ab5258cd0e19a200663837a9574d0f2d
peppe-99 <g.cardaropoli99@gmail.com>
2023-07-24 20:17:28 +0200

    code review, centralizest node & calculate execution time

--- src/DegreeCentrality/degree-centrality-GPU-CSR.cu
+++ src/DegreeCentrality/degree-centrality-GPU-CSR.cu
@@ -1,16 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<cuda.h>
 #include"../../include/utils.h"
 
 __global__ void dc_kernel(int n, int *R, int *C, double *dc);
 
 int main(int argc, char const *argv[]) {
     
-    int n, r_size, c_size;
+    int n, r_size, c_size, max = 0;
     int *h_r, *h_c;
     int *d_r, *d_c;
     double *h_dc, *d_dc;
+
+    float time;
+
+    cudaEvent_t start, stop;
 
     /* Input: numero di nodi e archi del grafo */
     printf("Number of nodes: ");
@@ -19,6 +23,7 @@ int main(int argc, char const *argv[]) {
 
     printf("Number of edges: ");
     scanf("%d", &c_size);
+    c_size *= 2;
 
     /* Allocazione strutture dati host */
     h_r = (int*)malloc(r_size * sizeof(int));
@@ -40,16 +45,39 @@ int main(int argc, char const *argv[]) {
     dim3 blockDim(64);
     dim3 gridDim((n + blockDim.x - 1) / blockDim.x);
 
+    /* Creiamo gli eventi per il tempo */
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start); // tempo di inizio
+
     /* Invocazione del kernel */
     dc_kernel<<<gridDim, blockDim>>>(n, d_r, d_c, d_dc);
+
+    /* Calcolo tempo di esecuzione */
+    cudaEventRecord(stop); // tempo di fine
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
 
     /* Copia dei risultati da device a host */
     cudaMemcpy(h_dc, d_dc, n * sizeof(double), cudaMemcpyDeviceToHost);
 
+    /* Individuazione del nodo più centrale */
+    for (int i = 0; i < n; i++) {
+        if (h_dc[i] > h_dc[max]) {
+            max = i;
+        }
+    }
+
     /* Stampa dei risultati */
     printf("\nDegree Centrality:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Score %d: %f\n", i+1, h_dc[i]);
+    printf("\nmax: %d - score: %f\n", max+1, h_dc[max]);
+    printf("\ntime: %f ms\n\n", time);
+
+    if (n <= 10) {
+        for (int i = 0; i < n; i++) {
+            printf("Score %d: %f\n", i+1, h_dc[i]);
+        }
     }
 
     /* free della memoria */
@@ -64,7 +92,7 @@ int main(int argc, char const *argv[]) {
 }
 
 __global__ void dc_kernel(int n, int *R, int *C, double *dc) {
-    int idx = threadIdx.x;
+    int idx = threadIdx.x + (blockDim.x * blockIdx.x);
 
     if (idx < n) {
         dc[idx] = (double) (R[idx+1] - R[idx]) / (n - 1);
