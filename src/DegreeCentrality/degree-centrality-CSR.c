8b92590be1c2ba1318495a814afa27ae45b20061
peppe-99 <g.cardaropoli99@gmail.com>
2023-07-24 18:45:12 +0200

    code review & calculate execution time

--- src/DegreeCentrality/degree-centrality-CSR.c
+++ src/DegreeCentrality/degree-centrality-CSR.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include"../../include/utils.h"
 
 int main(int argc, char const *argv[]) {
@@ -7,7 +8,10 @@ int main(int argc, char const *argv[]) {
     int *r, *c;
     double *d_c;
 
-    int n, r_size, c_size;
+    int n, r_size, c_size, max = 0;
+    double time;
+
+    clock_t begin, end;
 
     /* Input: numero di nodi e archi del grafo */
     printf("Number of nodes: ");
@@ -16,19 +20,39 @@ int main(int argc, char const *argv[]) {
 
     printf("Number of edges: ");
     scanf("%d", &c_size);
+    c_size *= 2;
     
     /* Allocazione delle strutture dati */
     r = (int*)malloc(r_size * sizeof(int));
     c = (int*)malloc(c_size * sizeof(int));
     d_c = (double*)malloc(n * sizeof(int));
 
-    readRCEgraph(r, c, r_size, c_size, "data/demo/row_offsets.dat", "data/demo/column_indices.dat");
+    readRCEgraph(r, c, r_size, c_size, "data/dense/16000/random_r.dat", "data/dense/16000/random_c.dat");
 
-    /* Calcolo e stampa della degree-centrality */
-    printf("\nDegree Centrality:\n");
+    begin = clock();
+
+    /* Calcolo della degree-centrality */
     for(int i = 0; i < r_size-1; i++) {
         d_c[i] = (double) (r[i+1] - r[i]) / (n-1);
-        printf("Score %d: %f\n", i, d_c[i]);
+        if (d_c[i] > d_c[max]) {
+            max = i;
+        }
+    }
+
+    end = clock();
+
+    /* Calcolo tempo di esecuzione */
+    time = (double) (end - begin) / CLOCKS_PER_SEC;
+
+    /* Stampa dei risultati */
+    printf("\nDegree Centrality\n");
+    printf("\nmax: %d - score: %f\n", max+1, d_c[max]);
+    printf("\ntime: %f ms\n\n", time * 1000);
+
+    if (n <= 10) {
+        for(int i = 0; i < n; i++) {
+            printf("Score %d: %f\n", i+1, d_c[i]);
+        }
     }
 
     /* free della memoria */
