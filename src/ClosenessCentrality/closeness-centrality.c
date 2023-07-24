46539451d3f0db7ef863a7091a8fed94e95b040d
peppe-99 <g.cardaropoli99@gmail.com>
2023-07-24 21:50:41 +0200

    code review, centralizest node & calculate execution time

--- src/ClosenessCentrality/closeness-centrality.c
+++ src/ClosenessCentrality/closeness-centrality.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 #include"../../include/utils.h"
 
 int main(int argc, char const *argv[]) {
 
-    int node, rows, cols, rimanenti;
+    int node, rows, cols, rimanenti, max = 0;
     int *matrix, *distance_matrix;
     double *closeness_centrality;
+    double time;
+
+    clock_t begin, end;
 
     /* Input: numero di nodi del grafo */
     printf("Number of nodes: ");
@@ -21,27 +25,42 @@ int main(int argc, char const *argv[]) {
     closeness_centrality = (double*)malloc(node * sizeof(double));
 
     /* Leggiamo la matrice di esempio da un file */
-    readIMatrix(rows, cols, matrix, "data/demo/matrix.dat");
+    readIMatrix(rows, cols, matrix, "data/sparse/8000/random_matrix.dat");
 
     /* Stampiamo la matrice di esempio */
     printIMatrix(rows, cols, matrix);
 
+    begin = clock();
+
     /* Calcoliamo la distance matrix */
     distanceMatrix(rows, cols, matrix, distance_matrix);
 
-    /* Stampiamo la distance matrix */
-    printf("\nDistance Matrix:\n");
-    printIMatrix(rows, cols, distance_matrix);
-
     /* Calcoliamo la Closeness Centrality */
-    printf("\nCloseness Centrality:\n");
     for (int i = 0; i < node; i++) {
         int sum_dist = 0;
         for (int j = 0; j < cols; j++) {
             sum_dist += distance_matrix[i * cols + j];
         }
         closeness_centrality[i] = (double) node / sum_dist;
-        printf("Score %d: %f\n", (i+1), closeness_centrality[i]);
+        if (closeness_centrality[i] > closeness_centrality[max]) {
+            max = i;
+        }
+    }
+
+    end = clock();
+
+    /* Calcolo tempo di esecuzione */
+    time = (double) (end - begin) / CLOCKS_PER_SEC;
+
+    /* Stampa dei risultati */
+    printf("\nCloseness Centrality\n");
+    printf("\nmax: %d - score: %f\n", max+1, closeness_centrality[max]);
+    printf("\ntime: %f ms\n\n", time * 1000);
+
+    if (node <= 10) {
+        for (int i = 0; i < node; i++) {
+            printf("Score %d: %f\n", (i+1), closeness_centrality[i]);
+        }
     }
 
     /* free della memoria */
